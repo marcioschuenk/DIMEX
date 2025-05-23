e8833853bc7d11dc79e1e847ded293bf39cda23b
Loreti <111466632+PedroLoreti@users.noreply.github.com>
2025-05-22 21:22:31 -0300

    atualizado a rota de sobras

--- backend/src/router/sobra.route.ts
+++ backend/src/router/sobra.route.ts
@@ -3,12 +3,6 @@ import { SobrasController } from "../controllers/sobras.controllers";
 
 export const routes = Router();
 
-export const sobrasRoutes = () => {
-  routes.post("/sobras", async (req, res, next) => {
-    try {
-      await new SobrasController().createSobras(req, res);
-    } catch (error) {
-      next(error);
-    }
-  });
-};
+const sobrasControllers = new SobrasController();
+
+routes.post("/", sobrasControllers.createSobras);
