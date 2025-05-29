4a519652966b01da4595262b78c5abe81dfa2ec3
Loreti <111466632+PedroLoreti@users.noreply.github.com>
2025-05-28 21:53:18 -0300

    Adicionado verificação de token nas rotas

--- backend/src/router/salanobre.route.ts
+++ backend/src/router/salanobre.route.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { SalaNobreController } from "../controllers/salanobre.controllers";
 import { IsValidBody } from "../middleware/isValidBody.middleware";
 import { createCaixasSchema } from "../schemas/salanobre.schema";
+import { verifyToken } from "../middleware/verifyToken.middleware";
 
 export const salaNobreRoutes = Router();
 
@@ -9,6 +10,3 @@ const salaNobreControllers = new SalaNobreController();
 
 salaNobreRoutes.post("/", IsValidBody.execute({ body: createCaixasSchema}), salaNobreControllers.createCaixas);
 salaNobreRoutes.get("/", salaNobreControllers.getCaixas);
-
-
-
--- backend/src/router/sobra.route.ts
+++ backend/src/router/sobra.route.ts
@@ -2,10 +2,11 @@ import { Router } from "express";
 import { SobrasController } from "../controllers/sobras.controllers";
 import { IsValidBody } from "../middleware/isValidBody.middleware";
 import { createSobrasSchema } from "../schemas/sobras.schemas";
+import { verifyToken } from "../middleware/verifyToken.middleware";
 
 export const sobrasRoutes = Router();
 
 const sobrasControllers = new SobrasController();
 
-sobrasRoutes.post("/", IsValidBody.execute({ body: createSobrasSchema}), sobrasControllers.createSobras);
-sobrasRoutes.get("/", sobrasControllers.getAllSobras);
+sobrasRoutes.post("/", verifyToken.execute, IsValidBody.execute({ body: createSobrasSchema}), sobrasControllers.createSobras);
+sobrasRoutes.get("/", verifyToken.execute, sobrasControllers.getAllSobras);
